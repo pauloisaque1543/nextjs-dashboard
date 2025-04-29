5890cc8f995a5bcaefe99a942ba83fa4b0e09791
pauloisaque1543 <pauloisaquecontatocomercial@gmail.com>
2025-04-29 10:42:41 -0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className="relative w-0 h-0 border-1-[15px] border-r-[15px] border-b-[15px] border-l-transparent  borde-r-transparent border-b-black "/>
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.!!</strong> This is the example for the{' '}
+            <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
